ef1845c9e6b1c25c0623cc0d2454d6c85b08f69d
RamyaRajalingam <114818091+RamyaRajalingam@users.noreply.github.com>
2022-10-04 11:24:30 +0300

    Add files via upload

---
+++ SecondLargestUsingArrays.java
@@ -0,0 +1,19 @@
+package week1.day2;
+import java.util.Arrays; 
+
+public class SecondLargestUsingArrays {
+	
+	public static void main(String[] args) {
+	
+		    int[] numbers = {12,8,6,4,45,75,88};
+			int length = numbers.length;
+			//System.out.println(length);
+			Arrays.sort(numbers);
+			System.out.println("second largest number is " + numbers[5]);	
+			
+	}
+
+}
+
+// if i put index no -2 showing error need to know how to resolve
+
